522a5b56c9be2f5818d5cc3553944af92e0a1f56
ZhangPeng <zp8360@qq.com>
2014-10-29 15:50:57 +0800

    swagger response

--- src/main/java/com/ctb/rest/disease/SelfCheckController.java
+++ src/main/java/com/ctb/rest/disease/SelfCheckController.java
@@ -14,6 +14,9 @@ import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.ctb.entity.disease.BodyPartDisease;
+import com.ctb.entity.disease.DiseaseDetailInfo;
+import com.ctb.entity.disease.DiseaseInfo;
 import com.ctb.service.disease.BodyPartDiseaseService;
 import com.ctb.service.disease.DiseaseDetailInfoService;
 import com.ctb.service.disease.DiseaseInfoService;
@@ -48,7 +51,7 @@ public class SelfCheckController {
 	@GET
 	@Path("/bodypart/{bodypart}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "通过身体具体部分获取疾病列表", notes = "通过身体具体部分获取疾病列表", response = JsonNode.class)
+	@ApiOperation(value = "通过身体具体部分获取疾病列表", notes = "通过身体具体部分获取疾病列表", response = BodyPartDisease.class,responseContainer="List")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "创建失败"), @ApiResponse(code = 404, message = "地址无效") })
 	public JsonNode bodyPart(
 			@ApiParam(value = "身体部位", required = true) @PathParam("bodypart") String bodypart
@@ -60,7 +63,7 @@ public class SelfCheckController {
 	@GET
 	@Path("/bodypart/type/{bodypart}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "通过身体大部分获取疾病列表", notes = "通过身体大部分获取疾病列表", response = JsonNode.class)
+	@ApiOperation(value = "通过身体大部分获取疾病列表", notes = "通过身体大部分获取疾病列表", response = BodyPartDisease.class,responseContainer="List")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "创建失败"), @ApiResponse(code = 404, message = "地址无效") })
 	public JsonNode bodyPartType(
 			@ApiParam(value = "身体部位", required = true) @PathParam("bodypart") String bodypart
@@ -71,7 +74,7 @@ public class SelfCheckController {
 	@GET
 	@Path("/bodypart/disease/page/{page}/size/{size}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "分页获取部位疾病信息", notes = "分页获取部位疾病信息", response = JsonNode.class)
+	@ApiOperation(value = "分页获取部位疾病信息", notes = "分页获取部位疾病信息", response = BodyPartDisease.class,responseContainer="List")
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "创建失败"), @ApiResponse(code = 404, message = "地址无效") })
 	public JsonNode getBodyPartDisease(
 			@ApiParam(value = "页数", allowableValues = "range[1,5]", required = true) @PathParam("page") String page,
@@ -83,7 +86,7 @@ public class SelfCheckController {
 	@GET
 	@Path("/disease/{code}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "通过Code获取疾病信息", notes = "通过Code获取疾病信息", response = JsonNode.class)
+	@ApiOperation(value = "通过Code获取疾病信息", notes = "通过Code获取疾病信息", response = DiseaseInfo.class)
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "创建失败"), @ApiResponse(code = 404, message = "地址无效") })
 	public JsonNode diseaseBycode(
 			@ApiParam(value = "疾病Code", required = true) @PathParam("code") String code
@@ -94,7 +97,7 @@ public class SelfCheckController {
 	@GET
 	@Path("/disease/detail/{code}")
 	@Produces(MediaType.APPLICATION_JSON)
-	@ApiOperation(value = "通过Code获取疾病详细信息", notes = "通过Code获取疾病详细信息", response = JsonNode.class)
+	@ApiOperation(value = "通过Code获取疾病详细信息", notes = "通过Code获取疾病详细信息", response = DiseaseDetailInfo.class)
 	@ApiResponses(value = { @ApiResponse(code = 400, message = "创建失败"), @ApiResponse(code = 404, message = "地址无效") })
 	public JsonNode diseaseDetailBycode(
 			@ApiParam(value = "疾病Code", required = true) @PathParam("code") String code
